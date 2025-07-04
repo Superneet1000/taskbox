910ce6fd3d037dbc9da9b8962fcc369e4a7f58bc
Superneet1000 <takahiro1098@icloud.com>
2025-07-04 13:58:06 +0900

    first commit

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from "@storybook/react";
 
+import { initialize, mswLoader } from "msw-storybook-addon";
+
+import '../src/index.css';
+
+initialize();
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +14,7 @@ const preview: Preview = {
       },
     },
   },
+  loaders:[mswLoader],
 };
 
 export default preview;
